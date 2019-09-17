60a75af38acd5efe671044fcb0e85378accaac39
Lucas Akira Uehara <lucas.uehara@riachuelo.com.br>
2019-09-17 14:43:32 -0300

    feat: reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, FormControl, FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gh-issues';
+  questionForm: FormGroup;
+
+  constructor() {
+    this.questionForm = new FormGroup({
+      question: new FormControl('test'),
+      answers: new FormArray([
+        new FormGroup({ text: new FormControl('1. answer'), value: new FormControl(false) }),
+        new FormGroup({ text: new FormControl('2. answer'), value: new FormControl(false) }),
+        new FormGroup({ text: new FormControl('3. answer'), value: new FormControl(true) }),
+      ]),
+      difficulty: new FormControl(0),
+      moreInformations: new FormControl('')
+    });
+  }
+
+  createQuestion(form) {
+    console.log(this.questionForm.value); // return many properties and not object with array
+  }
 }
